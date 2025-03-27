32c5d8312a657fe8b9551560f2b6e8c613e488cb
Ylandolsi <yesslandolsi@gmail.com>
2025-03-27 13:04:14 +0100

    update the refreshtoken saved in frontend

--- MemberOnly.Api/Models/RefreshTokenFront.cs
+++ MemberOnly.Api/Models/RefreshTokenFront.cs
@@ -5,16 +5,16 @@ namespace MemberOnly.Api.Models;
 
 public class RefreshTokenFront
 {
-            [Required]
+        [Required]
         public string UserName{ get; set; }
         
         [Required]
 
         public string RefreshToken { get; set; } = string.Empty;
         
-        public DateTime Expires { get; set; }
+        // DateTime Expires { get; set; }
         
-        public bool IsActive { get; set; } = true;
+        //public bool IsActive { get; set; } = true;
 
 
 }
